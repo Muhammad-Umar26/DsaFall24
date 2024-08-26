253708c48a203f15549d2b4da7ea59d369d82b07
Muhammad Umar <134300686+M-Umar4002@users.noreply.github.com>
2024-08-27 01:17:29 +0500

    Create q03.cpp

---
+++ Labs/02/q03.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+bool haveCommonFriend(bool arr[][5], int person1, int person2) {
+    for(int i = 0; i < 5; ++i) {
+        if(arr[i][person1] && arr[i][person2]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main () {
+    bool arr[5][5] = {
+        {false, true, false, true, true},
+        {true, false, true, false, true},
+        {false, true, false, false, false},
+        {true, false, false, false, true},
+        {true, true, false, true, false}
+    };
+
+    int person1 = 0, person2 = 4;
+
+    if(haveCommonFriend(arr, person1, person2)) {
+        cout << person1 << " and " << person2 << " have common friend(s)" << endl;
+    } else {
+        cout << person1 << " and " << person2 << " dont have common friends" << endl;
+    }
+    
+    return 0;
+}
