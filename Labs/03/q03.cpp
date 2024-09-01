52389b2ac6336e81858298593628daa09032cfb1
Muhammad Umar <134300686+M-Umar4002@users.noreply.github.com>
2024-09-02 00:21:18 +0500

    Create q03.cpp

---
+++ Labs/03/q03.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node* next;
+        
+        Node(int val = 0, Node* next = nullptr) : val(val), next(next) {}
+};
+
+class SinglyLinkedList {
+    Node* head;
+
+    public:
+        SinglyLinkedList() : head(nullptr) {}
+
+        void insertAtFront(int val) {
+            head = new Node(val, head);
+        }
+
+        void segregateEvenOdd() {
+            Node* evenHead = nullptr;
+            Node* oddHead = nullptr;
+            Node* evenCurr = nullptr;
+            Node* oddCurr = nullptr;
+
+            for(Node* curr = head; curr; curr = curr->next) {
+                if(curr->val & 1) {
+                    if(!oddHead) {
+                        oddHead = curr;
+                        oddCurr = curr;
+                    } else {
+                        oddCurr->next = curr;
+                        oddCurr = oddCurr->next;
+                    }
+                } else {
+                    if(!evenHead) {
+                        evenHead = curr;
+                        evenCurr = curr;
+                    } else {
+                        evenCurr->next = curr;
+                        evenCurr = evenCurr->next;
+                    }
+                }
+            }
+
+            if(evenCurr) evenCurr->next = oddHead;
+            if(oddCurr) oddCurr->next = nullptr;
+
+            head = evenHead ? evenHead : oddHead;
+        }
+
+        void printList() {
+            for(Node* curr = head; curr; curr = curr->next) {
+                cout << curr->val << " -> ";
+            }
+            cout << "NULL" << endl;
+        }
+};
+
+int main () {
+    SinglyLinkedList sll;
+    sll.insertAtFront(6);
+    sll.insertAtFront(7);
+    sll.insertAtFront(1);
+    sll.insertAtFront(4);
+    sll.insertAtFront(5);
+    sll.insertAtFront(10);
+    sll.insertAtFront(12);
+    sll.insertAtFront(8);
+    sll.insertAtFront(15);
+    sll.insertAtFront(17);
+
+    cout << "Original List" << endl;
+    sll.printList();
+
+    sll.segregateEvenOdd();
+
+    cout << endl << "Segregated List" << endl;
+    sll.printList();
+    
+    return 0;
+}
