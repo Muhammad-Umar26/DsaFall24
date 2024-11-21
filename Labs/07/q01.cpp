43cc41c0b8a8f024ffeed81ce13052c2e3500ed7
Muhammad Umar <134300686+Muhammad-Umar26@users.noreply.github.com>
2024-11-22 03:32:21 +0500

    Update q01.cpp

--- Labs/07/q01.cpp
+++ Labs/07/q01.cpp
@@ -1,139 +1,154 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-class Node {
-    Node* next;
-    Node* prev;
-    int val;
-public:
-    friend class DoublyLinkedList;
-    Node() : next(nullptr), prev(nullptr), val(0) {};
-    Node(Node* next, Node* prev, int v) : next(next), prev(prev), val(v) {};
-};
+class SinglyLinkedList {
+    struct Node {
+        int val;
+        Node* next;
 
-class DoublyLinkedList {
+        Node(int val = 0, Node* next = nullptr) : val(val), next(next) {}
+    };
+
     Node* head;
-public:
-    DoublyLinkedList() : head(nullptr) {};
 
-    void End(int v) {
-        Node* newnode = new Node(nullptr, nullptr, v);
-        if (!head) {
-            head = newnode;
+    void swap(Node* node1, Node* node2) {
+        if(!node1 || !node2 || node1 == node2) return;
+        
+        Node* prevNode1 = nullptr;
+        for(Node* curr = head; curr != node1; curr = curr->next) {
+            prevNode1 = curr;
         }
-        else {
-            Node* temp = head;
-            while (temp->next != nullptr) {
+        
+        Node* prevNode2 = nullptr;
+        for(Node* curr = head; curr != node2; curr = curr->next) {
+            prevNode2 = curr;
+        }
+        
+        if(node1->next == node2) {
+            node1->next = node2->next;
+            node2->next = node1;
+        } else {
+            Node* temp = node2->next;
+            node2->next = node1->next;
+            node1->next = temp;
+            
+            prevNode2->next = node1;
+        }
+        
+        if(prevNode1) {
+            prevNode1->next = node2;
+        } else {
+            head = node2;
+        }
+    }
+
+    Node* getTail(Node* curr){
+        while(curr && curr->next) {
+            curr = curr->next;
+        }
+        return curr;
+    }
+    
+    Node* partition(Node* head, Node* end, Node*& newHead, Node*& newEnd){
+        Node* pivot = end;
+        Node* prev = nullptr;
+        Node* curr = head;
+        Node* tail = pivot;
+
+        while(curr != pivot){
+            if (curr->val < pivot->val) {
+                if(!newHead) {
+                    newHead = curr;
+                }
+
+                prev = curr;
+                curr = curr->next;
+            } else {
+                if(prev) prev->next = curr->next;
+                Node* temp = curr->next;
+                curr->next = nullptr;
+                tail->next = curr;
+                tail = curr;
+                curr = temp;
+            }
+        }
+
+        if(!newHead) newHead = pivot;
+
+        newEnd = tail;
+
+        return pivot;
+    }
+
+    Node* quickSort(Node* head, Node* end) {
+        if(!head || head == end) return head;
+
+        Node* newHead = nullptr, *newEnd = nullptr;
+        Node* pivot = partition(head, end, newHead, newEnd);
+
+        if(newHead != pivot) {
+            Node* temp = newHead;
+            while(temp->next != pivot) {
                 temp = temp->next;
             }
-            temp->next = newnode;
-            newnode->prev = temp;
+            temp->next = nullptr;
+
+            newHead = quickSort(newHead, temp);
+
+            temp = getTail(newHead);
+            temp->next = pivot;
         }
+
+        pivot->next = quickSort(pivot->next, newEnd);
+
+        return newHead;
     }
 
-    void PrintList() {
-        Node* temp = head;
-        while (temp) {
-            cout << " <-> " << temp->val;
-            temp = temp->next;
+    public:
+        SinglyLinkedList() : head(nullptr) {}
+
+        void insertAtFront(int val) {
+            head = new Node(val, head);
         }
-        cout << endl;
-    }
 
-    ~DoublyLinkedList() {
-        while (head) {
-            Node* del = head;
-            head = head->next;
-            delete del;
+        void quickSort() {
+            head = quickSort(head, getTail(head));
         }
-    }
 
-    void swapNodes(Node* node1, Node* node2) {
-        if (node1 == node2) return;
-
-        // Swap the next pointers
-        Node* tempNext = node1->next;
-        node1->next = node2->next;
-        node2->next = tempNext;
-
-        // Swap the previous pointers
-        Node* tempPrev = node1->prev;
-        node1->prev = node2->prev;
-        node2->prev = tempPrev;
-
-        // Adjust next pointers of adjacent nodes
-        if (node1->next != nullptr) node1->next->prev = node1;
-        if (node2->next != nullptr) node2->next->prev = node2;
-
-        // Adjust previous pointers of adjacent nodes
-        if (node1->prev != nullptr) node1->prev->next = node1;
-        if (node2->prev != nullptr) node2->prev->next = node2;
-
-        // If one of them is the head
-        if (head == node1) head = node2;
-        else if (head == node2) head = node1;
-    }
-
-    Node* Partition(Node* L, Node* R) {
-        Node* pivot = L;
-        Node* i = L->next;
-        Node* j = R;
-
-        while (i != j->next) {
-            while (i != R && i->val <= pivot->val) {
-                i = i->next;
+        void printList() {
+            for(Node* curr = head; curr; curr = curr->next) {
+                cout << curr->val << " -> ";
             }
-            while (j != L && j->val > pivot->val) {
-                j = j->prev;
-            }
+            cout << "NULL" << endl;
+        }
 
-            if (i != j && i != j->next) {
-                swapNodes(i, j);
+        ~SinglyLinkedList() {
+            while(head) {
+                Node* temp = head;
+                head = head->next;
+                delete temp;
             }
         }
-        swapNodes(pivot, j);
-        return j; 
-    }
-
-    void QuickSortHelper(Node* L, Node* R) {
-        if (L == nullptr || R == nullptr || L == R || L == R->next) return;
-
-        Node* pivot = Partition(L, R);
-        QuickSortHelper(L, pivot->prev);    
-        QuickSortHelper(pivot->next, R);  
-    }
-
-    void QuickSort() {
-        if (!head || !head->next) return;
-        Node* tail = head;
-        while (tail->next) {
-            tail = tail->next;
-        }
-
-        QuickSortHelper(head, tail);
-    }
 };
 
-int main() {
-    DoublyLinkedList L1;
+int main () {
+    SinglyLinkedList sll;
+    sll.insertAtFront(3);
+    sll.insertAtFront(5);
+    sll.insertAtFront(1);
+    sll.insertAtFront(9);
+    sll.insertAtFront(8);
+    sll.insertAtFront(7);
+    sll.insertAtFront(10);
 
-    L1.End(5);
-    L1.End(4);
-    L1.End(35);
-    L1.End(2);
-    L1.End(11);
-    L1.End(77);
-    L1.End(30);
-    L1.End(24);
-    L1.End(10);
-    L1.End(7);
+    cout << "Initial list:" << endl;
+    sll.printList();
 
-    cout << "Before Sorting: ";
-    L1.PrintList();
+    sll.quickSort();
 
-    L1.QuickSort();
-
-    cout << "After Sorting: ";
-    L1.PrintList();
+    cout << endl << "After sorting" << endl;
+    sll.printList();
+    
     return 0;
 }
